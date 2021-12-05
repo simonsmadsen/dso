444db337d77aaaab3134f66b10804c79c62c3d71
Simon Madsen <sism@tv2.dk>
2021-12-05 12:07:12 +0100

    update deps

--- deps.ts
+++ deps.ts
@@ -2,18 +2,18 @@ export {
   assert,
   assertEquals,
   assertThrowsAsync,
-} from "https://deno.land/std@v0.51.0/testing/asserts.ts";
+} from "https://deno.land/std@0.51.0/testing/asserts.ts";
 export {
   Client,
   ClientConfig,
   Connection,
-} from "https://deno.land/x/mysql@2.1.0/mod.ts";
+} from "https://deno.land/x/mysql@v2.10.1/mod.ts";
 export {
   Join,
   Order,
   Query,
   replaceParams,
   Where,
-} from "https://deno.land/x/sql_builder@1.5.0/mod.ts";
+} from "https://deno.land/x/sql_builder@v1.9.1/mod.ts";
 
 import "./src/Reflect.ts";
